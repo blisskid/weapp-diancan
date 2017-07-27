02509a7d3ff6cb821268144e830ed2c933179f1a
blisskid <yuleirichard@163.com>
2017-07-27 17:22:32 +0800

    11

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,8 +1,75 @@
 Page({
-  data:{
-    
-    foodList:[],
-    detailFood:{},
+  data: {
+
+    foodList: [],
+    t_foodList: [
+      {
+        id: 1,
+        title: "黑胡椒意酱面",
+        cost: 45,
+        desc: "进口意大利通心粉制作，搭配有机番茄秘制酱汁。",
+        icon: "https://fuss10.elemecdn.com/8/05/0b0f3719bf1c9c1673ed69e262888jpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 2,
+        title: "吉士意大利虾仁面",
+        cost: 40,
+        desc: "进口意大利通心粉制作，搭配进口地中海大虾仁。",
+        icon: "https://fuss10.elemecdn.com/4/df/ff64bc5b06893a12aafb1e94f8b31jpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 3,
+        title: "牛排意大利面",
+        cost: 38,
+        desc: "进口意大利通心粉制作，搭配新鲜酱汁牛排和甜糯玉米。",
+        icon: "https://fuss10.elemecdn.com/3/42/70aae8406958d22657c5772e2412ejpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 4,
+        title: "香炸鸡翅",
+        cost: 12,
+        desc: "有机食品，绿色农场放心肉源",
+        icon: "https://fuss10.elemecdn.com/e/0e/df4f9d07b191d34ceddc3fedd88dcjpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 5,
+        title: "一品寿司",
+        cost: 22,
+        desc: "百分百手工，百分百口味，来自东海岸的问候。",
+        icon: "https://fuss10.elemecdn.com/8/53/353cf146fc9cab79479efcfb6e88ajpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 6,
+        title: "水果拼盘",
+        cost: 16,
+        desc: "新鲜时蔬，源自生活的百味奇珍。",
+        icon: "https://fuss10.elemecdn.com/0/b9/42b68495a09ec2e501ec3eaa36c6ejpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 7,
+        title: "巧手三明治",
+        cost: 22,
+        desc: "好吃不贵，明治选择。",
+        icon: "https://fuss10.elemecdn.com/a/fe/0d647946855f76e9dcdfbedfcad61jpeg.jpeg",
+        num: 0
+      },
+      {
+        id: 8,
+        title: "培根焗饭",
+        cost: 26,
+        desc: "好吃不解释，真的不解释。",
+        icon: "https://fuss10.elemecdn.com/9/de/30676686cf98d88961eb69f1f3083jpeg.jpeg",
+        num: 0
+      }
+
+    ],
+    detailFood: {},
     modalHidden: true,
 
     //订单数据
@@ -32,15 +99,15 @@ Page({
   },
 
   // 页面初始化 options为页面跳转所带来的参数
-  onLoad:function(options){
-    
+  onLoad: function (options) {
+
     //全局数据中获得食品列表
-    var appInstance = getApp();
-    var t_foodList = appInstance.globalData.foodList;
+    // var appInstance = getApp();
+    // var t_foodList = appInstance.globalData.foodList;
 
     var t_arr;
-    for(var i=0; i<t_foodList.length; i++){
-      if(i%2 == 0){
+    for (var i = 0; i < t_foodList.length; i++) {
+      if (i % 2 == 0) {
         t_arr = [];
         this.data.foodList.push(t_arr); //整理成二维数据，方便显示
       }
@@ -49,17 +116,17 @@ Page({
   },
 
   //关闭食品详情弹窗
-  closeModal: function(e) {
+  closeModal: function (e) {
     this.setData({
       modalHidden: true
     })
   },
 
   //展示食品详情弹窗
-  imageClick:function(e){
+  imageClick: function (e) {
 
     var dataset = e.currentTarget.dataset;
-    
+
     var appInstance = getApp();
     var t_foodList = appInstance.globalData.foodList;
 
@@ -72,17 +139,17 @@ Page({
   },
 
   //提交订单
-  sublimitClick:function(e){
+  sublimitClick: function (e) {
 
     //订单列表 传参
     var agrs = JSON.stringify(this.data.orderList);
     wx.navigateTo({
-        url: '../order/order?order=' + agrs
+      url: '../order/order?order=' + agrs
     })
   },
 
   //加入到购物车
-  addCartClick:function(e){
+  addCartClick: function (e) {
     var dataset = e.currentTarget.dataset;
     this.changeNum(dataset.index, true);
 
@@ -92,7 +159,7 @@ Page({
 
     //1s后关闭
     var _this = this;
-    setTimeout(function(){
+    setTimeout(function () {
       _this.setData({
         toastHidden: true
       });
@@ -103,18 +170,18 @@ Page({
   },
 
   //增加数量
-  addClick:function(e){
+  addClick: function (e) {
     var dataset = e.currentTarget.dataset;
     this.changeNum(dataset.index, true);
   },
 
   //减少数量
-  reduceClick:function(e){
+  reduceClick: function (e) {
     var dataset = e.currentTarget.dataset;
     this.changeNum(dataset.index, false);
   },
 
-  changeNum:function(index, bool){
+  changeNum: function (index, bool) {
     var appInstance = getApp();
     var t_food = appInstance.globalData.foodList[index];
 
@@ -123,34 +190,34 @@ Page({
     var obj = orderList[t_food.id];
 
     //如果存在，则数量变化
-    if(obj){
-        if(bool){
-          obj.num = obj.num + 1;
-        }else{
-          if(obj.num > 0){
-            obj.num = obj.num - 1;
-          }else{
-            return;//已经减少为0
-          }
+    if (obj) {
+      if (bool) {
+        obj.num = obj.num + 1;
+      } else {
+        if (obj.num > 0) {
+          obj.num = obj.num - 1;
+        } else {
+          return;//已经减少为0
         }
-    }else{
-        if(bool){
-          //不存在，点击增加，则写入一条订单数据，数量默认1
-          obj = {
-            id: t_food.id,
-            num: 1,
-            cost: t_food.cost,
-            title: t_food.title
-          };
-          this.data.orderList[t_food.id] = obj;
-        }else{
-          return;//不存在，并且点击的是减少
-        }
+      }
+    } else {
+      if (bool) {
+        //不存在，点击增加，则写入一条订单数据，数量默认1
+        obj = {
+          id: t_food.id,
+          num: 1,
+          cost: t_food.cost,
+          title: t_food.title
+        };
+        this.data.orderList[t_food.id] = obj;
+      } else {
+        return;//不存在，并且点击的是减少
+      }
     }
-    
+
     var order_num = 0;
     var order_cost = 0;
-    for(var k in orderList){
+    for (var k in orderList) {
       order_num = orderList[k].num + order_num; //计算总数量
       order_cost = order_cost + orderList[k].cost * orderList[k].num; //计算总价格
     }
@@ -163,7 +230,7 @@ Page({
   },
 
   //食品类型
-  foodTypeChange: function(e) {
+  foodTypeChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       foodTypesIndex: e.detail.value
@@ -171,7 +238,7 @@ Page({
   },
 
   //排序类型
-  rankTypeChange: function(e) {
+  rankTypeChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       rankTypesIndex: e.detail.value
